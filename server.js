54f99fcd9b2809d925be3dd916eef641dcbe63a9
unknown <batoul.ghosn@isae.edu.lb>
2024-01-01 11:49:19 +0200

    addition of two lines in server

--- server.js
+++ server.js
@@ -15,7 +15,11 @@ app.get ("/",(req,res)=>{
 });
 const userRoutes = require("./routes/userRoutes");
 app.use("/users", userRoutes);
-
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send({ success: false, message: 'Internal Server Error', error: err });
+   });
+   
 app.listen(8000,()=>{
    console.log('server is running on port:8000')
 })
