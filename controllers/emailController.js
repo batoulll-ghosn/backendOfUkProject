83c328f0ae0f7824af07d8eb861a0d77379e3b10
unknown <batoul.ghosn@isae.edu.lb>
2024-01-09 18:41:44 +0200

    .

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -54,7 +54,59 @@ const sendEmail = async (req, res) => {
         });
     }
 };
+const onRegisterEmail  = async (req, res) => {
+    try {
+        const { email,content } = req.body;
 
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASSWORD,
+            },
+        });
+
+        const mailOptions = {
+            from: process.env.EMAIL_USER,
+            to: email,
+            subject: 'Regarding the subscription in the United Kingdom for Education and Learning',
+            html: `<p>Greetings</p>
+            <p>We hope this email finds you as well!</p>
+            <p style="padding: 12px; border-left: 4px solid #d0d0d0; font-style: italic;">
+              you Have registered to ${content} </p>
+              <p>To confirm your registeration you need to transfer the price mentioned using <b> YOUR EMAIL <b> on the following account 0000 1111 </p>
+            <p>
+                Sincerely,<br>The United Kingdom For Education and Learning Public Relations' team
+            </p>`,
+        };
+
+        try {
+            await transporter.sendMail(mailOptions);
+            console.log('Email sent successfully');
+
+            res.status(200).json({
+                success: true,
+                message: 'Email sent successfully',
+            });
+        } catch (error) {
+            console.error('Error sending email:', error);
+
+            res.status(500).json({
+                success: false,
+                message: 'Unable to send email',
+                error: error,
+            });
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({
+            success: false,
+            message: 'An unexpected error occurred',
+            error: error,
+        });
+    }
+};
 module.exports = {
     sendEmail,
+    onRegisterEmail,
 };
--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -3,8 +3,9 @@ const router = express.Router();
 
 const {
     sendEmail,
+    onRegisterEmail,
 } = require('../controllers/emailController');
 
 router.post('/send', sendEmail);
-
+router.post('/registerToConf', onRegisterEmail)
 module.exports = router;
