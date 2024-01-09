2d56bcf15970db350ecb02e104bb41201920b2d9
unknown <batoul.ghosn@isae.edu.lb>
2024-01-09 18:09:28 +0200

    .

--- controllers/engageToConf.js
+++ controllers/engageToConf.js
@@ -1,24 +1,32 @@
 const dbb = require("../config/connection");
 const EngageToConf = async (req, res) => {
-    const {confrence_id, user_id} = req.body;
-  
-    try {
-        const [result] = await dbb.query(
-            `INSERT INTO engagedtoconfrence(confrence_id, user_id) VALUES ('${confrence_id}','${user_id}')`
-        );
-  
-        res.status(200).json({
-            success: true,
-            message: "Engagement to conference was successfull",
-            data: result,
-        });
-    } catch (error) {
-        res.status(400).json({
-            success: false,
-            message: "Engagement to conference was not successfull",
-            error:error.toString(),
-        });
-    }
-  };
+   const {confrence_id, user_id, paid} = req.body;
+ 
+   try {
+       // Check if a row with the given user_id and conference_id already exists
+       const [existingRow] = await dbb.query(
+           `SELECT EXISTS(SELECT 1 FROM engagedtoconfrence WHERE confrence_id='${confrence_id}' AND user_id='${user_id}') AS exists`
+       );
+       
+       if (existingRow[0].exists) {
+           throw new Error("You have already submitted to this conference.");
+       } else {
+           const [result] = await dbb.query(
+               `INSERT INTO engagedtoconfrence(confrence_id, user_id, paid) VALUES ('${confrence_id}','${user_id}','${paid}')`
+           );
+           res.status(200).json({
+               success: true,
+               message: "Engagement to conference was successful",
+               data: result,
+           });
+       }
+   } catch (error) {
+       res.status(400).json({
+           success: false,
+           message: "Engagement to conference was not successful",
+           error: error.toString(),
+       });
+   }
+};
 
-module.exports={EngageToConf}
+module.exports={EngageToConf};
