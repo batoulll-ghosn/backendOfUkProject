4300102155a8be240cbe106770758d495a0bd845
unknown <batoul.ghosn@isae.edu.lb>
2024-01-10 18:56:58 +0200

    get the conference you engaged to

--- routes/engageToConf.js
+++ routes/engageToConf.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const control = require("../controllers/engageToConf");
-router.get('/getEngagedConf', control.getEnngagedConfWhereUser);
+router.get('/getEngagedConf/user_id', control.getEnngagedConfWhereUser);
 router.post("/engageToConference", control.EngageToConf);
 module.exports = router;
