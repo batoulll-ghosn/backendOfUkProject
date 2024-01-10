5d3e58e3157b256072b4f1da864afa54d691f10c
unknown <batoul.ghosn@isae.edu.lb>
2024-01-10 18:49:22 +0200

    get the conference you engaged to

--- controllers/engageToConf.js
+++ controllers/engageToConf.js
@@ -3,7 +3,6 @@ const EngageToConf = async (req, res) => {
     const {confrence_id, user_id} = req.body;
     const paid =0
     try {
-        // Check if a row with the given user_id and conference_id already exists
         const [existingRow] = await dbb.query(
             `SELECT * FROM engagedtoconfrence WHERE confrence_id='${confrence_id}' AND user_id='${user_id}'`
         );
@@ -29,5 +28,25 @@ const EngageToConf = async (req, res) => {
     }
   };
   
-
-module.exports={EngageToConf};
+const getEnngagedConfWhereUser = async (req, res) => {
+    const {user_id} = req.body;
+    try {
+      const [result] = await dbb.query(`SELECT confrences.*, engagedtoconfrence.paid, engagedtoconfrence.user_id
+      FROM confrences 
+          LEFT JOIN engagedtoconfrence ON engagedtoconfrence.confrence_id = confrences.id
+      WHERE engagedtoconfrence.paid = '1' AND engagedtoconfrence.user_id = ${user_id}`);
+      res.status(200).json({
+        success: true,
+        message: "Conference data retrieved successfully",
+        data: result,
+      });
+    } catch (error) {
+      res.status(400).json({
+        success: false,
+        message: "Unable to get the Conference",
+        error,
+      });
+    }
+   };
+   
+module.exports={EngageToConf,getEnngagedConfWhereUser};
--- routes/engageToConf.js
+++ routes/engageToConf.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const control = require("../controllers/engageToConf");
+router.get('/getEngagedConf', control.getEnngagedConfWhereUser);
 router.post("/engageToConference", control.EngageToConf);
 module.exports = router;
