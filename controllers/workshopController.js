a0a07c757717255d2f3d2f4cb88260a677f4833a
unknown <batoul.ghosn@isae.edu.lb>
2024-01-11 00:04:29 +0200

    engagement to workshop

--- controllers/workshopController.js
+++ controllers/workshopController.js
@@ -15,4 +15,32 @@ const getAllWorkshops = async (req, res) => {
       });
     }
   };
-  module.exports={getAllWorkshops}
+  const EngageToWorkshop = async (req, res) => {
+    const {workshop_id, user_id} = req.body;
+    const paid =0
+    try {
+        const [existingRow] = await dbb.query(
+            `SELECT * FROM engagedtoworkshop WHERE workshop_id='${workshop_id}' AND user_id='${user_id}'`
+        );
+        
+        if (existingRow.length > 0) {
+            throw new Error("You have already submitted to this workshop.");
+        } else {
+            const [result] = await dbb.query(
+                `INSERT INTO engagedtoworkshop (workshop_id, user_id, paid) VALUES ('${workshop_id}','${user_id}','${paid}')`
+            );
+            res.status(200).json({
+                success: true,
+                message: "Engagement to workshop was successful",
+                data: result,
+            });
+        }
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            message: "Engagement to Workshop was not successful",
+            error: error.toString(),
+        });
+    }
+  };
+  module.exports={getAllWorkshops,EngageToWorkshop}
