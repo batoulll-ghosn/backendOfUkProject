06e31ee603b1a340f6e5cbe59f28aeaad0326104
unknown <batoul.ghosn@isae.edu.lb>
2024-01-15 18:52:41 +0200

    checking payment

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -4,6 +4,7 @@ const upload = multer({ storage: multer.memoryStorage() });
 const router = express.Router();
 const control = require("../controllers/courseController");
 router.get("/getAll", control.getAllCourses);
+router.get("/getAllWh", control.getAllCoursesWh);
 router.get("/getByLanguageName/:languageName", control.getAllCoursesByName);
 router.get("/getByType/:type", control.getAllCoursesByType);
 router.get("/getByLanguageLevel/:level", control.getAllCoursesByLevel);
