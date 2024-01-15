a0d25cbc20052735e2d299eb93a939152eef1e6a
unknown <batoul.ghosn@isae.edu.lb>
2024-01-15 20:36:11 +0200

    checking payment to nott paid

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -13,6 +13,7 @@ router.post('/AddCourse',upload.single('img'), control.AddCourse);
 router.post('/AddSchedule/:course_id/:day/:hour',control.AddSchedule);
 router.put('/EditCourse/:id',upload.single('img'),control.UpdateCourse);
 router.put('/UpdateToPaid/:email',control.updatePaidStatus);
+router.put('/UpdateToNotPaid/:email',control.updateNOTPaidStatus);
 router.get('/getEngagedCourseSchedule/:user_id', control.getEngagedCourseWhereUser);
 router.get('/getSchedule', control.getSchedule);
 router.delete('/deleteCourse/:id',control.deleteCourse);
