c9bc1c14d7a88d99a8b2778732f9cf3e18928937
unknown <batoul.ghosn@isae.edu.lb>
2024-01-18 13:44:40 +0200

    l

--- extra/generateToken.js
+++ extra/generateToken.js
@@ -1,9 +1,7 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 exports.generateToken = (id, role) => {
-  const token = jwt.sign({ id, role }, 'batoul@#123456789', {
-    expiresIn: '1h',
+  return jwt.sign({ id, role }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
   });
-  console.log(token);
-  return token;
 };
